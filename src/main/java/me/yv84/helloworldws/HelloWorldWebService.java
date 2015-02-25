eafb059e14da2eafa52996fab78d1f4b0430be81
Vladimir Yudintsev <git@yv84.me>
2015-02-25 13:32:53 +0500

    deploy to glassfish

--- src/main/java/me/yv84/helloworldws/HelloWorldWebService.java
+++ src/main/java/me/yv84/helloworldws/HelloWorldWebService.java
@@ -2,14 +2,15 @@ package me.yv84.helloworldws;
 
 
 import java.lang.String;
+import java.util.List;
 import java.util.ArrayList;
 
-public class HelloWorldWebService{
+public class HelloWorldWebService {
     public List<String> getList() {
         List<String> l = new ArrayList<String>();
         l.add("Table 1");
-        l.add('Table 2');
-        l.add('Table 3');
+        l.add("Table 2");
+        l.add("Table 3");
         return l;
     }
 }
